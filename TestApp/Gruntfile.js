fc6b10882a54e3e8883194aedc73f4bd34a3dfa6
Terry Mace <terence.h.mace@gmail.com>
2024-08-27 20:48:46 +0100

    Client side libraries are now installed using NPM

---
+++ TestApp/Gruntfile.js
@@ -0,0 +1,50 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "node_modules/bootstrap",
+                        src: ["**"],
+                        dest: "wwwroot/lib/bootstrap/",
+                    },
+                    {
+                        expand: true,
+                        cwd: "node_modules/@fortawesome/fontawesome-free/scss",
+                        src: ["**"],
+                        dest: "wwwroot/lib/font-awesome/scss",
+                    },
+                    {
+                        expand: true,
+                        cwd: "node_modules/@fortawesome/fontawesome-free/webfonts",
+                        src: ["**"],
+                        dest: "wwwroot/lib/font-awesome/webfonts",
+                    },
+                    {
+                        expand: true,
+                        cwd: "node_modules/jquery",
+                        src: ["**"],
+                        dest: "wwwroot/lib/jquery",
+                    },
+                    {
+                        expand: true,
+                        cwd: "node_modules/jquery-validation/dist",
+                        src: ["*"],
+                        dest: "wwwroot/lib/jquery-validation",
+                    },
+                    {
+                        expand: true,
+                        cwd: "node_modules/jquery-validation-unobtrusive/dist",
+                        src: ["*"],
+                        dest: "wwwroot/lib/jquery-validation-unobtrusive",
+                    },
+                ],
+            },
+        },
+        clean: ["wwwroot/lib/"],
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+};
